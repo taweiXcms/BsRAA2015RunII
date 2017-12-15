b9346738bb3559d72d3dea5d699d83cea2c91c7c
Ta-Wei <ta-wei.wang@cern.ch>
2017-12-16 00:57:49 +0100

    update

---
+++ CrossSection/CopyToAN.sh
@@ -0,0 +1,20 @@
+#cp plotReweight/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotReweight/
+#cp plotFitsComp/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFitsComp/
+#cp plotClosure/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotClosure/
+#cp plotEff/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotEff/
+#cp plotEff_1050/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotEff_1050/
+#cp plotFONLL/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFONLL/
+#cp plotRAA/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotRAA/
+#cp ../AcceptanceUncertainty_1050-old/step2_GettheRatio/Acc* ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Sys/SysAcc/
+#cp ../AcceptanceUncertainty_1050-old/make_1bin_result/step6_getAccSys/Bs_accvar* ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Sys/SysAcc/
+#cp ../Systematics/SystematicSummaryPP.pdf ../Systematics/SystematicSummaryPbPb_Cent100.pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Sys/
+#cp -r ../varSelection/2ndOrd/FitStrategy_pm0p2/plotRatios ../varSelection/2ndOrd/FitStrategy_pm0p2/plotFits ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Sys/SysCut/
+cp plotChecksPrompt/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotChecksPrompt/
+
+#cp plotFits/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFits/
+#cp plotFitsOnSaved/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFitsOnSaved/
+#cp plotFits_1050/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFits_1050/
+#cp plotFitsOnSaved_1050/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotFitsOnSaved_1050/
+#cp plotCrossSection/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotCrossSection/
+#cp plotCrossSection_1050/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotCrossSection_1050/
+#cp plotRAA/*pdf ~/public/MyText/HIN-17-008-BsRAA5p02/notes/AN-17-210/trunk/Plots/Results/plotRAA/
