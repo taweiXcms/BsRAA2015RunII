db02c9501d8962fe7024b838e919a3d988d01b1d
Ta-wei Wang <tawei@submit-hi2.mit.edu>
2018-03-09 02:16:36 -0500

    update

---
+++ Others/tkPtPreCutStudy/do.sh
@@ -0,0 +1,15 @@
+cp tkPtPreCutStudy-norm/plotFits/plotFits/data_pp_1.pdf ./data_pp_norm.pdf
+cp tkPtPreCutStudy-pt0p60p9/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt0p60p9.pdf
+cp tkPtPreCutStudy-pt0p71p0/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt0p71p0.pdf
+cp tkPtPreCutStudy-pt0p81p1/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt0p81p1.pdf
+cp tkPtPreCutStudy-pt0p91p2/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt0p91p2.pdf
+cp tkPtPreCutStudy-pt1p01p3/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt1p01p3.pdf
+cp tkPtPreCutStudy-pt1p11p4/plotFits/plotFits/data_pp_1.pdf ./data_pp_pt1p11p4.pdf
+
+cp tkPtPreCutStudy-norm/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_norm.pdf
+cp tkPtPreCutStudy-pt0p60p9/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt0p60p9.pdf
+cp tkPtPreCutStudy-pt0p71p0/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt0p71p0.pdf
+cp tkPtPreCutStudy-pt0p81p1/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt0p81p1.pdf
+cp tkPtPreCutStudy-pt0p91p2/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt0p91p2.pdf
+cp tkPtPreCutStudy-pt1p01p3/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt1p01p3.pdf
+cp tkPtPreCutStudy-pt1p11p4/plotFits/plotFits/data_PbPb_1.pdf ./data_PbPb_pt1p11p4.pdf
