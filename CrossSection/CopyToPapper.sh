b46f6fb8e5001a64612174ba69ca33fbd1f5777f
Ta-Wei <ta-wei.wang@cern.ch>
2018-05-10 04:24:04 +0200

    update

--- CrossSection/CopyToPapper.sh
+++ CrossSection/CopyToPapper.sh
@@ -1,12 +1,12 @@
-des="~/public/MyText/HIN-17-008-BsRAA5p02/papers/HIN-17-008/trunk/"
+des="/afs/cern.ch/user/t/twang/public/MyText/HIN-17-008-BsRAA5p02/papers/HIN-17-008/trunk"
 #des="paperPlots"
-cp plotFits/plotFitsOnSaved_roofit/data_pp_1.pdf $des/Figure_001-a.pdf
-cp plotFits/plotFitsOnSaved_roofit/data_PbPb_1.pdf $des/Figure_001-b.pdf
-cp plotFits/plotFitsOnSaved_roofit/data_pp_2.pdf $des/Figure_002-a.pdf
-cp plotFits/plotFitsOnSaved_roofit/data_PbPb_2.pdf $des/Figure_002-b.pdf
-cp plotCrossSection/plotCrossSection_750/canvasSigmaBplusRatiopp.pdf $des/Figure_003.pdf
-cp plotCrossSection/plotCrossSection/canvasSigmaBplusRatiopp_AddPbPb.pdf $des/Figure_004-a.pdf
-cp plotRAA/canvasRAAPbPb_0_100_BpRAA.pdf $des/Figure_004-b.pdf
-cp plotBsBpRatio/BsBpRatio.pdf $des/Figure_005.pdf
+#cp plotFits/plotFitsOnSaved_roofit/data_pp_1.pdf $des/Figure_001-a.pdf
+#cp plotFits/plotFitsOnSaved_roofit/data_PbPb_1.pdf $des/Figure_001-b.pdf
+#cp plotFits/plotFitsOnSaved_roofit/data_pp_2.pdf $des/Figure_002-a.pdf
+#cp plotFits/plotFitsOnSaved_roofit/data_PbPb_2.pdf $des/Figure_002-b.pdf
+#cp plotCrossSection/plotCrossSection_750/canvasSigmaBplusRatiopp.pdf $des/Figure_003.pdf
+#cp plotCrossSection/plotCrossSection/canvasSigmaBplusRatiopp_AddPbPb.pdf $des/Figure_004-a.pdf
+#cp plotRAA/canvasRAAPbPb_0_100_BpRAA.pdf $des/Figure_004-b.pdf
+#cp plotBsBpRatio/BsBpRatio.pdf $des/Figure_005.pdf
 cp plotFits/plotFitsOnSaved_roofit_Inc/data_pp_1.pdf $des/Figure_006-a.pdf
 cp plotFits/plotFitsOnSaved_roofit_Inc/data_PbPb_1.pdf $des/Figure_006-b.pdf
